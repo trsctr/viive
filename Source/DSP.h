5c3eb2e953ed571a050e3c4f9160ac685792ba11
Otto Andelin <trisector@gmail.com>
2026-01-09 10:38:17 +0200

    fix bug in onePoleLowpassCoeff

--- Source/DSP.h
+++ Source/DSP.h
@@ -6,10 +6,11 @@ inline float onePoleLowpassCoeff(float timeConstantMs, float sampleRate) {
 	float tau = timeConstantMs / 1000.0f;
 	jassert(tau > 0.0f); // tau can't be zero
 	jassert(sampleRate > 0.0f); // sample rate can't be zero
-	return 1.0f - std::exp(-1.0f / tau * sampleRate);
+	float x = std::exp(-1.0f / (tau * sampleRate));
+	return 1.0f - x;
 }
 
 inline float onePoleLowpass(float input, float& state, float coeff) {
-	state += coeff * (input - state);
+	state += (input - state) * coeff;
 	return state;
 }
