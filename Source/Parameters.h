84c48aebe61d4dc4bb0bb181c5fb756e40343c4f
Otto Andelin <trisector@gmail.com>
2026-01-10 19:15:17 +0200

    declare params for stereo width and effect amt

--- Source/Parameters.h
+++ Source/Parameters.h
@@ -5,6 +5,8 @@ const juce::ParameterID gainParamID{ "gain", 1 };
 const juce::ParameterID delayTimeParamID{ "delayTime", 1 };
 const juce::ParameterID mixParamID{ "mix", 1 };
 const juce::ParameterID feedbackParamID{ "feedback", 1 };
+const juce::ParameterID stereoParamID{ "stereo", 1 };
+const juce::ParameterID effectAmtParamID{ "effectAmt", 1 };
 const juce::ParameterID lowCutFreqParamID{ "lowCutFreq", 1 };
 const juce::ParameterID lowCutQParamID{ "lowCutQ", 1 };
 const juce::ParameterID highCutFreqParamID{ "highCutFreq", 1 };
@@ -31,6 +33,8 @@ public:
 	float delayTime() const { return m_delayTime; }
 	float mix() const { return m_mix; }
 	float feedback() const { return m_feedback; }
+	float stereo() const { return m_stereo; }
+	float effectAmt() const { return m_effectAmt; }
 	float lowCutFreq() const { return m_lowCutFreq; }
 	float lowCutQ() const { return m_lowCutQ; }
 	float highCutFreq() const { return m_highCutFreq; }
@@ -41,6 +45,8 @@ private:
 	juce::AudioParameterFloat* m_delayTimeParam;
 	juce::AudioParameterFloat* m_mixParam;
 	juce::AudioParameterFloat* m_feedbackParam;
+	juce::AudioParameterFloat* m_stereoParam;
+	juce::AudioParameterFloat* m_effectAmtParam;
 	juce::AudioParameterFloat* m_lowCutFreqParam;
 	juce::AudioParameterFloat* m_lowCutQParam;
 	juce::AudioParameterFloat* m_highCutFreqParam;
@@ -49,6 +55,8 @@ private:
 	juce::LinearSmoothedValue<float> m_gainSmoother;
 	juce::LinearSmoothedValue<float> m_mixSmoother;
 	juce::LinearSmoothedValue<float> m_feedbackSmoother;
+	juce::LinearSmoothedValue<float> m_stereoSmoother;
+	juce::LinearSmoothedValue<float> m_effectAmtSmoother;
 	juce::LinearSmoothedValue<float> m_lowCutFreqSmoother;
 	juce::LinearSmoothedValue<float> m_lowCutQSmoother;
 	juce::LinearSmoothedValue<float> m_highCutFreqSmoother;
@@ -61,6 +69,8 @@ private:
 	float m_delayTime = 0.0f;
 	float m_mix = 0.5f;
 	float m_feedback = 0.0f;
+	float m_stereo = 0.0f;
+	float m_effectAmt = 0.0f;
 	float m_lowCutFreq = 20.0f;
 	float m_lowCutQ = defaultFilterQ;
 	float m_highCutFreq = 20000.0f;
