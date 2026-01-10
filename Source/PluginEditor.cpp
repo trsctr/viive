966ee1cd3be24b3bf51fcda51c047313eb0c7bc0
Otto Andelin <trisector@gmail.com>
2026-01-10 23:09:32 +0200

    add new knobs to editor, change output group layout

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -19,8 +19,9 @@ ViiveAudioProcessorEditor::ViiveAudioProcessorEditor (ViiveAudioProcessor& p)
     m_delayGroup.setTextLabelPosition(juce::Justification::horizontallyCentred);
     m_delayGroup.addAndMakeVisible(m_delayTimeKnob);
     m_delayGroup.addAndMakeVisible(m_feedbackKnob);
+	m_delayGroup.addAndMakeVisible(m_stereoKnob);
+	m_delayGroup.addAndMakeVisible(m_effectAmtKnob);
     addAndMakeVisible(m_delayGroup);
-
 
     m_filterGroup.setText("Filter");
     m_filterGroup.setTextLabelPosition(juce::Justification::horizontallyCentred);
@@ -36,7 +37,7 @@ ViiveAudioProcessorEditor::ViiveAudioProcessorEditor (ViiveAudioProcessor& p)
 	m_outputGroup.addAndMakeVisible(m_gainKnob);
     addAndMakeVisible(m_outputGroup);
 
-    setSize (550, 330);
+    setSize (650, 330);
 }
 
 ViiveAudioProcessorEditor::~ViiveAudioProcessorEditor()
@@ -56,18 +57,20 @@ void ViiveAudioProcessorEditor::resized()
     int y = 10;
 	int height = bounds.getHeight() - 20;
 
-    m_outputGroup.setBounds(bounds.getWidth() - 140, y, 130, height);
+    m_outputGroup.setBounds(bounds.getWidth() - 240, y, 200, height / 2 - 10);
     m_delayGroup.setBounds(10, y, m_outputGroup.getX() - 20, height / 2 - 10);
     m_filterGroup.setBounds(10, height / 2 + 10, m_outputGroup.getX() - 20, height / 2);
     
 	m_delayTimeKnob.setTopLeftPosition(20, 20);
 	m_feedbackKnob.setTopLeftPosition(m_delayTimeKnob.getRight() + 20, 20);
+	m_stereoKnob.setTopLeftPosition(m_feedbackKnob.getRight() + 20, 20);
+	m_effectAmtKnob.setTopLeftPosition(m_stereoKnob.getRight() + 20, 20);
 	m_lowCutFreqKnob.setTopLeftPosition(20, 20);
 	m_lowCutQKnob.setTopLeftPosition(m_lowCutFreqKnob.getRight() + 20, 20);
 	m_highCutFreqKnob.setTopLeftPosition(m_lowCutQKnob.getRight() + 20, 20);
 	m_highCutQKnob.setTopLeftPosition(m_highCutFreqKnob.getRight() + 20, 20);
 	m_mixKnob.setTopLeftPosition(20, 20);
-	m_gainKnob.setTopLeftPosition(20, m_mixKnob.getBottom() + 20);
+	m_gainKnob.setTopLeftPosition(m_mixKnob.getRight() + 20, 20);
 
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -29,15 +29,17 @@ public:
 private:
     ViiveAudioProcessor& m_audioProcessor;
 
+    RotaryKnob m_gainKnob{ "Gain", m_audioProcessor.apvts, gainParamID.getParamID() };
     RotaryKnob m_delayTimeKnob{ "Time", m_audioProcessor.apvts, delayTimeParamID.getParamID() };
+    RotaryKnob m_mixKnob{ "Mix", m_audioProcessor.apvts, mixParamID.getParamID() };
     RotaryKnob m_feedbackKnob{ "Feedback", m_audioProcessor.apvts, feedbackParamID.getParamID() };
+    RotaryKnob m_stereoKnob{ "Stereo", m_audioProcessor.apvts, stereoParamID.getParamID() };
+    RotaryKnob m_effectAmtKnob{ "Effect Amt", m_audioProcessor.apvts, effectAmtParamID.getParamID() };
     RotaryKnob m_lowCutFreqKnob{ "Low Cut Freq", m_audioProcessor.apvts, lowCutFreqParamID.getParamID() };
     RotaryKnob m_lowCutQKnob{ "Low Cut Q", m_audioProcessor.apvts, lowCutQParamID.getParamID() };
     RotaryKnob m_highCutFreqKnob{ "High Cut Freq", m_audioProcessor.apvts, highCutFreqParamID.getParamID() };
     RotaryKnob m_highCutQKnob{ "High Cut Q", m_audioProcessor.apvts, highCutQParamID.getParamID() };
-    RotaryKnob m_mixKnob{ "Mix", m_audioProcessor.apvts, mixParamID.getParamID() };
-    RotaryKnob m_gainKnob{ "Gain", m_audioProcessor.apvts, gainParamID.getParamID() };
-
+    
     juce::GroupComponent m_delayGroup, m_filterGroup, m_outputGroup;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ViiveAudioProcessorEditor)
