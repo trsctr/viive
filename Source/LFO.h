11802c2bae3f99da49855da465550a9042e835b3
Otto Andelin <trisector@gmail.com>
2026-02-13 11:00:50 +0200

    define lfo class

--- Source/LFO.h
+++ Source/LFO.h
@@ -1,11 +1,26 @@
-/*
-  ==============================================================================
-
-    LFO.h
-    Created: 13 Feb 2026 10:55:12am
-    Author:  trise
-
-  ==============================================================================
-*/
-
 #pragma once
+#include <JuceHeader.h>
+
+enum class LFOShape { Sine };
+class LFO {
+public:
+	LFO();
+	~LFO() = default;
+
+	void prepareToPlay(float sampleRate) noexcept;
+	void reset() noexcept;
+	float process();
+
+private:
+	LFOShape m_shape = LFOShape::Sine;
+
+	float m_sampleRate = 0.0f;
+
+	float m_frequency = 1.0f;
+	float m_phase = 0.0f;
+	float m_offset = 0.0f;
+	float m_increment = 0.0f;
+
+	void setIncrement();
+	float processSine(float phase);
+};
