a97a3310ccc7f475dd7d0f47e3d28182817117f1
Otto Andelin <trisector@gmail.com>
2026-02-23 12:34:09 +0200

    organize chorus related knobs to their own group

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,20 +23,23 @@ private:
     ViiveAudioProcessor& m_audioProcessor;
     LevelMeter m_meter;
 
-    RotaryKnob m_gainKnob{ "Gain", m_audioProcessor.apvts, gainParamID.getParamID() };
     RotaryKnob m_delayTimeKnob{ "Time", m_audioProcessor.apvts, delayTimeParamID.getParamID() };
-    RotaryKnob m_mixKnob{ "Mix", m_audioProcessor.apvts, mixParamID.getParamID() };
     RotaryKnob m_feedbackKnob{ "Feedback", m_audioProcessor.apvts, feedbackParamID.getParamID() };
     RotaryKnob m_stereoKnob{ "Stereo", m_audioProcessor.apvts, stereoParamID.getParamID() };
-    RotaryKnob m_effectAmtKnob{ "Effect Amt", m_audioProcessor.apvts, effectAmtParamID.getParamID() };
+
     RotaryKnob m_lowCutFreqKnob{ "Low Cut Freq", m_audioProcessor.apvts, lowCutFreqParamID.getParamID() };
     RotaryKnob m_lowCutQKnob{ "Low Cut Q", m_audioProcessor.apvts, lowCutQParamID.getParamID() };
     RotaryKnob m_highCutFreqKnob{ "High Cut Freq", m_audioProcessor.apvts, highCutFreqParamID.getParamID() };
     RotaryKnob m_highCutQKnob{ "High Cut Q", m_audioProcessor.apvts, highCutQParamID.getParamID() };
-	RotaryKnob m_fxParam1Knob{ "FX Param 1", m_audioProcessor.apvts, fxParam1ParamID.getParamID() };
-	RotaryKnob m_fxParam2Knob{ "FX Param 2", m_audioProcessor.apvts, fxParam2ParamID.getParamID() };
 
-    juce::GroupComponent m_delayGroup, m_filterGroup, m_outputGroup, m_fxGroup;
+    RotaryKnob m_gainKnob{ "Gain", m_audioProcessor.apvts, gainParamID.getParamID() };
+    RotaryKnob m_mixKnob{ "Mix", m_audioProcessor.apvts, mixParamID.getParamID() };
+
+    RotaryKnob m_chorusIntensityKnob{ "Intensity", m_audioProcessor.apvts, chorusIntensityParamID.getParamID() };
+	RotaryKnob m_chorusModRateKnob{ "Mod Rate", m_audioProcessor.apvts, chorusModRateParamID.getParamID() };
+	RotaryKnob m_chorusModDepthKnob{ "Mod Depth", m_audioProcessor.apvts, chorusModDepthParamID.getParamID() };
+
+    juce::GroupComponent m_delayGroup, m_filterGroup, m_outputGroup, m_chorusGroup;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ViiveAudioProcessorEditor)
 };
