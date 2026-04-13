3a0f4786e273764b136857612f9cc06bba505721
Otto Andelin <trisector@gmail.com>
2026-04-13 14:13:42 +0300

    add files and define stereodelay class

---
+++ Source/StereoDelay.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <JuceHeader.h>
+
+enum Channel { Left = 0, Right = 1 };
+
+class StereoDelay {
+public:
+	StereoDelay();
+	~StereoDelay() = default;
+
+	void prepareToPlay(double sampleRate, int samplesPerBlock) noexcept;
+	void reset() noexcept;
+	void processSample(const float &in, float &out, Channel channel);
+
+	void setDelayTime(float ms, Channel channel);
+	void setDelayTime(float ms);
+
+private:
+	juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> m_delayLines[2];
+
+	float m_delayTimes[2] = { 0.0f, 0.0f };
+
+	float m_sampleRate = 0.0f;
+
+	void setDelayTimeSamples(float samples, Channel channel);
+};
