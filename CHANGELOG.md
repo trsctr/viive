a9e645fc29a14e064f7e182f76e798357012eac0
Otto Andelin <112075238+trsctr@users.noreply.github.com>
2026-05-20 14:55:42 +0300

    Feat: Lofi effect (#10)
    * add bitcrusher class w downsampling function definition and declarations for other features
    
    * add bit reduction to bitcrusher
    
    * refactor BitCrusher class to a dsp primitive to be used by LofiEngine class
    
    * use round instead of trunc in bit reduction to avoid feedback self oscillation due to dc offset
    
    * add and implement lofiengine class, add lofi control parameters
    
    * change bitcrusher processing order: first downsample, then bitreduce
    
    * fix copypaste fail in LofiEngine.h, update changelog
    
    * fix max samplerate parameter typo, make bitreduce and downsample functions private, remove samplersperblock param from lofiengine prepare function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,15 @@
 
 All notable changes to this project will be documented in this file.
 
+## \[Unreleased]
+
+### Added
+
+* BitCrusher DSP primitive
+  * Performs first downsampling and then bit reduction
+* LofiEngine class which handles Left and Right channel BitCrushers and parameters
+* Lofi Mix, SampleRate and BitDepth parameters
+
 ## \[0.2.0] - 2026-05-18
 
 ### Added
---
+++ Source/LofiEngine.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include "BitCrusher.h"
+
+class LofiEngine
+{
+public:
+    LofiEngine();
+    ~LofiEngine() = default;
+
+    void prepareToPlay(double sampleRate) noexcept;
+    void reset() noexcept;
+    void processSample(const float& inL, const float& inR, float& outL, float& outR);
+    void update(const Parameters& params);
+
+private:
+    BitCrusher m_bitCrusherL{ 12.0f, 22050.0f };
+    BitCrusher m_bitCrusherR{ 12.0f, 22050.0f };
+
+    float m_sampleRate = 0.0f;
+    float m_mixLevel = 1.0f;
+    float m_bitDepth = 0.0f;
+    float m_downsampleRate = 0.0f;
+
+    void setBitDepth(float bitDepth);
+    void setDownsampleRate(float downsampleRate);
+    void setMixLevel(float mixLevel);
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(LofiEngine)
+};
