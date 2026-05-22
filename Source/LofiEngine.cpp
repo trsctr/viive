dde6485abdd107dce2b816d17a839e2de673bfaa
Otto Andelin <trisector@gmail.com>
2026-05-22 14:33:13 +0300

    lofiengine: lock bit depth to 8, add hp and lp filters

--- Source/LofiEngine.cpp
+++ Source/LofiEngine.cpp
@@ -1,5 +1,5 @@
 #include "LofiEngine.h"
-
+#include "DSP.h"
 LofiEngine::LofiEngine()
 {
 }
@@ -9,36 +9,42 @@ void LofiEngine::prepareToPlay(double sampleRate, int samplesPerBlock) noexcept
     m_sampleRate = static_cast<float>(sampleRate);
     m_bitCrusherL.prepareToPlay(m_sampleRate);
     m_bitCrusherR.prepareToPlay(m_sampleRate);
+    m_highpass.prepareToPlay(m_sampleRate, samplesPerBlock);
+    m_lowpass.prepareToPlay(m_sampleRate, samplesPerBlock);
+    m_highpass.setCutoff(100.0f); // remove low-end rumble that happens on low sample rates
+    m_highpass.setQ(0.3f);
+    m_lowpass.setCutoff(Parameters::maxLofiDampenFreq);
+    m_lowpass.setQ(0.2f);
 }
 
 void LofiEngine::reset() noexcept
 {
     m_bitCrusherL.reset();
     m_bitCrusherR.reset();
+    m_highpass.reset();
+    m_lowpass.reset();
+    m_mixLevel = 1.0f;
+    m_downsampleRate = -1.0f;
+    m_dampenFreq = -1.0f;
 }
 
 void LofiEngine::update(const Parameters& params)
 {
-    setBitDepth(params.lofiBitDepth());
     setDownsampleRate(params.lofiSampleRate());
     setMixLevel(params.lofiMixLevel());
+    setDampenFreq(params.lofiDampenFreq());
 }
 
 void LofiEngine::processSample(const float& inL, const float& inR, float& outL, float& outR)
 {
-    float processedL = m_bitCrusherL.processSample(inL);
-    float processedR = m_bitCrusherR.processSample(inR);
+    float processedL = 0.0f;
+    float processedR = 0.0f;
+    m_highpass.processSample(inL, inR, processedL, processedR);
+    processedL = m_bitCrusherL.processSample(processedL);
+    processedR = m_bitCrusherR.processSample(processedR);
+    m_lowpass.processSample(processedL, processedR, processedL, processedR);
     outL = inL * (1.0f - m_mixLevel) + processedL * m_mixLevel;
     outR = inR * (1.0f - m_mixLevel) + processedR * m_mixLevel;
-}
-
-void LofiEngine::setBitDepth(float bitDepth)
-{
-    if (!juce::approximatelyEqual(bitDepth, m_bitDepth)) {
-        m_bitDepth = bitDepth;
-        m_bitCrusherL.setBitDepth(m_bitDepth);
-        m_bitCrusherR.setBitDepth(m_bitDepth);
-    }
 }
 
 void LofiEngine::setDownsampleRate(float downsampleRate)
@@ -47,6 +53,14 @@ void LofiEngine::setDownsampleRate(float downsampleRate)
         m_downsampleRate = downsampleRate;
         m_bitCrusherL.setDownsampleRate(m_downsampleRate);
         m_bitCrusherR.setDownsampleRate(m_downsampleRate);
+    }
+}
+
+void LofiEngine::setDampenFreq(float dampenFreq)
+{
+    if (!juce::approximatelyEqual(dampenFreq, m_dampenFreq)) {
+        m_dampenFreq = dampenFreq;
+        m_lowpass.setCutoff(m_dampenFreq);
     }
 }
 
--- Source/LofiEngine.h
+++ Source/LofiEngine.h
@@ -4,6 +4,8 @@
 #include "BitCrusher.h"
 #include "Parameters.h"
 #include "InsertEffect.h"
+#include "StereoFilter.h"
+
 
 class LofiEngine : public InsertEffect
 {
@@ -17,17 +19,19 @@ public:
     void update(const Parameters& params) override;
 
 private:
-    BitCrusher m_bitCrusherL{ 12.0f, 22050.0f };
-    BitCrusher m_bitCrusherR{ 12.0f, 22050.0f };
+    BitCrusher m_bitCrusherL{ 8.0f, 22050.0f };
+    BitCrusher m_bitCrusherR{ 8.0f, 22050.0f };
+    StereoFilter m_highpass { FilterType::highpass };
+    StereoFilter m_lowpass { FilterType::lowpass };
 
     float m_sampleRate = 0.0f;
     float m_mixLevel = 1.0f;
-    float m_bitDepth = 0.0f;
-    float m_downsampleRate = 0.0f;
+    float m_downsampleRate = -1.0f;
+    float m_dampenFreq = -1.0f;
 
-    void setBitDepth(float bitDepth);
     void setDownsampleRate(float downsampleRate);
     void setMixLevel(float mixLevel);
+    void setDampenFreq(float dampenFreq);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(LofiEngine)
 };
